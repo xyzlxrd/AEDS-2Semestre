9dc563dd80a19161fb62db2d133533cc99db0e75
luiz <luizfelipe.carreir@gmail.com>
2025-03-25 17:56:50 -0300

    Update Q2Lista2.java

--- psc-lista-02-2025-main/Q2Lista2.java
+++ psc-lista-02-2025-main/Q2Lista2.java
@@ -1,7 +1,7 @@
 package com.mycompany.psclista012025;
 import java.util.Scanner;
 
-public class Psclista012025 {
+public class Q2Lista2 {
     
     public static void main(String[] args) {
         
